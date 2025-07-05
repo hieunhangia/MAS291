52bc7b7b488e5674c3bf763ae36cc637bb14857e
hieunhangia <hieuvippro531@gmail.com>
2025-07-05 13:48:53 +0700

    optimal code in 3.1

--- Question 3/Part 1.cs
+++ Question 3/Part 1.cs
@@ -4,13 +4,13 @@ public abstract class Part1
 {
     public static void Result(double a, double b, double x)
     {
-        FirstContact(a,b,x,
+        FirstContact(a, b, x,
             out var xFirstContactPoint, out var yFirstContactPoint, out var angleFirstContactPoint);
         Console.WriteLine($"Output of firstContact({a},{b},{x})" +
                           $" = ({xFirstContactPoint},{yFirstContactPoint},{angleFirstContactPoint})");
     }
 
-    private static void FirstContact(double xBall, double yBall, double angleBall,
+    public static void FirstContact(double xBall, double yBall, double angleBall,
         out double xFirstContactPoint, out double yFirstContactPoint, out double angleFirstContact)
     {
         var a = Math.Tan(angleBall);
@@ -43,52 +43,45 @@ public abstract class Part1
     private static double ComputeReflectAngle
         (double xVectorA, double yVectorA, double xVectorB, double yVectorB)
     {
-        return ComputeAngleOfTwoVector(xVectorA, yVectorA, xVectorB, yVectorB) > Math.PI / 2
-            ? Math.PI - ComputeAngleOfTwoVector(xVectorA, yVectorA, xVectorB, yVectorB)
-            : ComputeAngleOfTwoVector(xVectorA, yVectorA, xVectorB, yVectorB);
+        var angleOfTwoVector = ComputeAngleOfTwoVector(xVectorA, yVectorA, xVectorB, yVectorB);
+        return angleOfTwoVector > Math.PI / 2 ? Math.PI - angleOfTwoVector : angleOfTwoVector;
     }
 
     private static string RailOfFirstContact(double xBall, double yBall, double angleBall)
     {
-        ComputeAngle(xBall, yBall, out var o, out var a, out var b, out var c);
-        if (angleBall >= o && angleBall <= a)
-        {
-            return "OA";
-        }
+        ComputeAngleOfBallAndVertexOfSquareWithOx
+            (xBall, yBall, out var o, out var a, out var b, out var c);
+        if (angleBall >= o && angleBall <= a) return "OA";
 
-        if (angleBall >= b && angleBall <= c)
-        {
-            return "BC";
-        }
+        if (angleBall >= b && angleBall <= c) return "BC";
 
-        if (angleBall >= c && angleBall <= o)
-        {
-            return "CO";
-        }
+        if (angleBall >= c && angleBall <= o) return "CO";
 
         return "AB";
     }
 
-
-    private static void ComputeAngle(double xBall, double yBall,
+    private static void ComputeAngleOfBallAndVertexOfSquareWithOx(double xBall, double yBall,
         out double o, out double a, out double b, out double c)
     {
-        o = 2 * Math.PI - ComputeAngleOfBallWithSquareVertex(xBall, yBall, 0, 0);
-        a = 2 * Math.PI - ComputeAngleOfBallWithSquareVertex(xBall, yBall, 1, 0);
-        b = ComputeAngleOfBallWithSquareVertex(xBall, yBall, 1, 1);
-        c = ComputeAngleOfBallWithSquareVertex(xBall, yBall, 0, 1);
+        o = 2 * Math.PI - ComputeAngleOfBallAndVertexWithOx(xBall, yBall, 0, 0);
+        a = 2 * Math.PI - ComputeAngleOfBallAndVertexWithOx(xBall, yBall, 1, 0);
+        b = ComputeAngleOfBallAndVertexWithOx(xBall, yBall, 1, 1);
+        c = ComputeAngleOfBallAndVertexWithOx(xBall, yBall, 0, 1);
     }
 
-    private static double ComputeAngleOfBallWithSquareVertex(double xBall, double yBall, int xVertex, int yVertex)
-    {
-        var xVectorOfBallToVertex = xVertex - xBall;
-        var yVectorOfBallToVertex = yVertex - yBall;
-        return ComputeAngleOfTwoVector(xVectorOfBallToVertex, yVectorOfBallToVertex, 1, 0);
-    }
+    private static double ComputeAngleOfBallAndVertexWithOx
+        (double xBall, double yBall, int xVertex, int yVertex)
+        => ComputeAngleOfTwoVector(xVertex - xBall, yVertex - yBall, 1, 0);
 
     private static double ComputeAngleOfTwoVector
         (double xVectorA, double yVectorA, double xVectorB, double yVectorB)
-        => Math.Acos((xVectorA * xVectorB + yVectorA * yVectorB) /
-                     (Math.Sqrt(Math.Pow(xVectorA, 2) + Math.Pow(yVectorA, 2)) *
-                      Math.Sqrt(Math.Pow(xVectorB, 2) + Math.Pow(yVectorB, 2))));
+        => Math.Acos(
+            (xVectorA * xVectorB + yVectorA * yVectorB)
+            /
+            (
+                Math.Sqrt(xVectorA * xVectorA + yVectorA * yVectorA)
+                *
+                Math.Sqrt(xVectorB * xVectorB + yVectorB * yVectorB)
+            )
+        );
 }
