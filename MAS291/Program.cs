313053fe34d0347d95a1fd27e0ec2ae487badee0
hieunhangia <hieuvippro531@gmail.com>
2025-07-05 21:42:39 +0700

    99% done

--- MAS291/Program.cs
+++ MAS291/Program.cs
@@ -4,6 +4,8 @@ internal abstract class Program
 {
     private static void Main()
     {
-        Question_3.Part2.Result(100000);
+        Question_1.Part2.Result(0.5, 1000000);
+        Question_2.Part2.Result(10, 100000);
+        Question_3.Part2.Result(500000);
     }
 }
